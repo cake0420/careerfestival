35ea05d1bd4861d1eca61a2169c73bf2d65c4850
jeonggeon <kjkkmk42397@gmail.com>
2024-01-10 21:10:21 +0900

    errorsstatus dto 행사관련

--- src/main/java/careerfestival/career/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/careerfestival/career/apiPayload/code/status/ErrorStatus.java
@@ -16,8 +16,16 @@ public enum ErrorStatus implements BaseErrorCode {
 
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401","인증이 필요합니다."),
 
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403","금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403","금지된 요청입니다."),
+    // MemberMission relative exception
+    ONGOING_MISSION(HttpStatus.BAD_REQUEST, "MEMBER_MISSION4001", "이미 진행 중인 미션입니다."),
 
+    NOT_ONGOING_MISSION(HttpStatus.BAD_REQUEST, "MEMBER_MISSION4002", "진행 중인 행사가 아닙니다."),
+
+    MEMBER_MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_MISSION4003", "해당 행사가 존재하지 않습니다."),
+
+    // ex
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
 
     // 멤버 관련 응답
 
