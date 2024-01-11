86bfad43f1b4855bae6ea18530e2cf7b860d72e2
jeonggeon <kjkkmk42397@gmail.com>
2024-01-11 14:20:37 +0900

    #2 temp test 용 errorstatus, controller service 추가

--- src/main/java/careerfestival/career/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/careerfestival/career/apiPayload/code/status/ErrorStatus.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
     // common response
-
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500","서버 에러, 관리자에게 문의 바람니다."),
 
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400","잘못된 요청입니다."),
@@ -17,19 +16,15 @@ public enum ErrorStatus implements BaseErrorCode {
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401","인증이 필요합니다."),
 
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403","금지된 요청입니다."),
-    // MemberMission relative exception
-    ONGOING_MISSION(HttpStatus.BAD_REQUEST, "MEMBER_MISSION4001", "이미 진행 중인 미션입니다."),
+    // event relative
+    NOT_ONGOING_EVENT(HttpStatus.BAD_REQUEST, "EVENT4001", "진행 중인 행사가 아닙니다."),
 
-    NOT_ONGOING_MISSION(HttpStatus.BAD_REQUEST, "MEMBER_MISSION4002", "진행 중인 행사가 아닙니다."),
-
-    MEMBER_MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_MISSION4003", "해당 행사가 존재하지 않습니다."),
+    MEMBER_EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_EVENT4002", "해당 행사가 존재하지 않습니다."),
 
     // ex
-    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
-
-    // 멤버 관련 응답
-
-    // ~~~ 관련 응답 ....
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
+    // For test
+    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");
 
 
     private final HttpStatus httpStatus;
