03b0e2664b1ccb68b5a83b03e317db5b6ce75db3
SoowanPark <jack6767@kau.kr>
2024-01-16 18:15:37 +0900

    [Fix] RecordKeyWord.class bug

--- src/main/java/careerfestival/career/domain/mapping/RecordKeyword.java
+++ src/main/java/careerfestival/career/domain/mapping/RecordKeyword.java
@@ -1,6 +1,7 @@
 package careerfestival.career.domain.mapping;
 
 import careerfestival.career.domain.Record;
+import careerfestival.career.domain.User;
 import careerfestival.career.domain.common.BaseEntity;
 import careerfestival.career.domain.enums.KeywordName;
 import jakarta.persistence.*;
@@ -17,13 +18,17 @@ import lombok.NoArgsConstructor;
 public class RecordKeyword extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "RECORDKEYWORD_ID")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "RECORD_ID")
-    private Record record;
 
     @Enumerated(EnumType.STRING)
     private KeywordName keywordName;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id")
+    private Record record;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
